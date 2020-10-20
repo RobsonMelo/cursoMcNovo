259c01db0ece36dfe9ab21326215b1e2d43f6430
Robson Melo <robson.info2@gmail.com>
2020-10-19 22:42:38 -0300

    primeira classe dominio

---
+++ src/main/java/com/example/cursoMc/resources/CategoriaResource.java
@@ -0,0 +1,27 @@
+package com.example.cursoMc.resources;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.cursoMc.domain.Categoria;
+
+@RestController
+@RequestMapping(value="/Categorias")
+public class CategoriaResource {
+
+	@RequestMapping(method=RequestMethod.GET)
+	public List<Categoria> listar() {
+		Categoria c1 = new Categoria(1,"Refrigerante");
+		Categoria c2 = new Categoria(2,"Lanches");
+		
+		List<Categoria> listCategoria = new ArrayList();
+		listCategoria.add(c1);
+		listCategoria.add(c2);
+		
+		return listCategoria;
+	}
+}
